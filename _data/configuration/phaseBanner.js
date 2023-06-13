1e33efd6194616508b8446a4ec30b03669ea7fd5
Keith Kennedy <keith.kennedy@digital.homeoffice.gov.uk>
2023-06-13 09:22:51 +0100

    Removed phase banner configuration file

--- _data/configuration/phaseBanner.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  tag: {
-    text: "Alpha"
-  },
-  html: 'This is a new service – your <a class="govuk-link" target="_blank" href="https://www.homeofficesurveys.homeoffice.gov.uk/s/8PDDG2/">feedback (opens in a new tab)</a> will help us to improve it.'
-}
