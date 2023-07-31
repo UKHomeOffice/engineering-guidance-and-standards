0fa2d7acf22702a1378072699c2bc9099667dbec
Jeffrey Horton <jeff.horton@digital.homeoffice.gov.uk>
2023-07-31 11:17:10 +0100

    Alternative fix for #209 - support path prefix in related links

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,8 +1,33 @@
 // noinspection JSUnusedGlobalSymbols
 
+function mapRelatedSections(sections, pathPrefix) {
+  return sections.map(
+    section => ({
+      ...section,
+      items: (section.items ?? []).map(item => ({
+        ...item,
+        href: item.href?.startsWith('/') && pathPrefix ? `${pathPrefix}${item.href}` : item.href,
+      })),
+      subsections: mapRelatedSections(section.subsections ?? [], pathPrefix),
+    })
+  );
+}
+
 module.exports = {
   eleventyExcludeFromCollections: ({page, eleventyExcludeFromCollections}) => {
     return eleventyExcludeFromCollections || page.templateSyntax === 'scss';
+  },
+  // the path prefix isn't applied to related link urls by xGovukRelatedNavigation - so map them here
+  related: ({related, pathPrefix}) => {
+    console.log({related, pathPrefix})
+
+    if (!related) {
+      return related;
+    }
+
+    related.sections = mapRelatedSections(related.sections ?? [related], pathPrefix);
+
+    return related;
   }
 }
 
