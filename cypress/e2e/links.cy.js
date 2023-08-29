c156cb7be0a8b6a48234ac85ccd520012766f991
Keith Kennedy <keith.kennedy@digital.homeoffice.gov.uk>
2023-08-29 15:52:54 +0100

    URL checks will ignore 401 (unauthorised) and 403 (forbidden) error codes

--- cypress/e2e/links.cy.js
+++ cypress/e2e/links.cy.js
@@ -28,25 +28,17 @@ it('Check for invalid footer links', () => {
 function checkAllLinks() {
   // Exclude mailto links and exclude survey link -- there is rate limiting for the survey page
   cy.get("main a:not([href*='mailto:']):not([href*='homeofficesurveys']").each(page => {
-    cy.request(page.prop('href'))
+    checkUrl(page.prop('href'))
   })
 }
 
-function checkAllLinksOfListedPages() {
-  // Get each link in main page list (e.g. list of standards)
-  cy.get("#main-content ol a").each(link => {
-    cy.visit(link.prop('href'))
-    checkAllLinks()
-    cy.go('back')
-  })
-
-  cy.get('#main-content').then(mainContent => {
-    // Check if we can locate the "next" pagination link
-    if (mainContent.find(".govuk-pagination__next a").length > 0) {
-      cy.get("#main-content .govuk-pagination__next a").then(link => {
-        cy.visit(link.prop('href'))
-        checkAllLinksOfListedPages()
-      })
-    }
+function checkUrl(url) {
+  cy.request({
+    url : url,
+    failOnStatusCode:false,
+  }).then((resp) => {
+    expect(resp.status).to.not.eq(400)
+    expect(resp.status).to.not.eq(404)
+    expect(resp.status).to.not.gt(405)
   })
 }
