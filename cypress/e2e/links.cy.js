eac4066d3b6cef41b819bc5b2604c81b17a6661d
Keith Kennedy <keith.kennedy@digital.homeoffice.gov.uk>
2023-08-29 14:41:04 +0100

    Split link check for main pages into separate tests

--- cypress/e2e/links.cy.js
+++ cypress/e2e/links.cy.js
@@ -1,11 +1,28 @@
 import {testing_params} from "../support/testing_params";
 
-it('Check for invalid links on main pages', () => {
-  const pages = ['/', '/principles/', '/patterns/', '/standards/', '/tags/']
-  pages.forEach(page => {
-    cy.visit(testing_params.TEST_ROOT_URL + page)
-    checkAllLinks()
-  })
+it('Check for invalid links on home page', () => {
+  cy.visit(testing_params.TEST_ROOT_URL)
+  checkAllLinks()
+})
+
+it('Check for invalid links on patterns page', () => {
+  cy.visit(testing_params.TEST_ROOT_URL + '/patterns/')
+  checkAllLinks()
+})
+
+it('Check for invalid links on standards page', () => {
+  cy.visit(testing_params.TEST_ROOT_URL + '/standards/')
+  checkAllLinks()
+})
+
+it('Check for invalid links on principles page', () => {
+  cy.visit(testing_params.TEST_ROOT_URL + '/principles/')
+  checkAllLinks()
+})
+
+it('Check for invalid links on tags page', () => {
+  cy.visit(testing_params.TEST_ROOT_URL + '/tags/')
+  checkAllLinks()
 })
 
 it('Check for invalid links on content pages', () => {
