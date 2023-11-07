9527809a01c8d9d6b5235807b03417ed42c9897a
Keith Kennedy <133027753+keithkennedyHO@users.noreply.github.com>
2023-11-07 11:34:39 +0000

    Removed tech debt related to path prefix usages (#334)
    * Removed path prefix usages
    
    * Removed test path prefix for e2e workflow
    
    * Removed tag page logic used to work around path prefix issue

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,44 +1,6 @@
-// noinspection JSUnusedGlobalSymbols
-const { applyBaseToUrl } = require('@11ty/eleventy/src/Plugins/HtmlBasePlugin')
-
-function mapRelatedSections(sections, pathPrefix) {
-  return sections.map(
-    section => ({
-      ...section,
-      items: (section.items ?? []).map(item => ({
-        ...item,
-        href: applyBaseToUrl(item.href, pathPrefix, {pathPrefix}),
-      })),
-      subsections: mapRelatedSections(section.subsections ?? [], pathPrefix),
-    })
-  );
-}
-
 module.exports = {
   eleventyExcludeFromCollections: ({page, eleventyExcludeFromCollections}) => {
     return eleventyExcludeFromCollections || page.templateSyntax === 'scss';
-  },
-  // the path prefix isn't applied to related link urls by xGovukRelatedNavigation - so map them here
-  pagination: ({pagination, pathPrefix}) => {
-    if (!pagination || Object.keys(pagination).length === 0) {
-      return pagination;
-    }
-    if (pagination.href.previous) {
-      pagination.href.previous = applyBaseToUrl(pagination.href.previous, pathPrefix, {pathPrefix});
-    }
-    if (pagination.href.next) {
-      pagination.href.next = applyBaseToUrl(pagination.href.next, pathPrefix, {pathPrefix});
-    }
-    return pagination;
-  },
-  related: ({related, pathPrefix}) => {
-    if (!related) {
-      return related;
-    }
-
-    related.sections = mapRelatedSections(related.sections ?? [related], pathPrefix);
-
-    return related;
   }
 }
 
