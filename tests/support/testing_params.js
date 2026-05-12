7c19ea9d816b48c145e5da343d850b67a3c8ca7b
Paul Nattrass <paul.nattrass1@homeoffice.gov.uk>
2026-05-12 13:24:15 +0100

    fix: update TEST_URL format in e2e tests workflow

--- tests/support/testing_params.js
+++ tests/support/testing_params.js
@@ -2,8 +2,12 @@ export const testing_params = {
   TEST_URL: process.env.TEST_URL ?? 'http://127.0.0.1',
   TEST_PORT: process.env.TEST_PORT ?? '8080',
   TEST_PATH: process.env.TEST_PATH ?? '',
+
+  get NORMALIZED_TEST_URL() {
+    return /^https?:\/\//i.test(this.TEST_URL) ? this.TEST_URL : `http://${this.TEST_URL}`;
+  },
   
   get TEST_ROOT_URL() {
-    return `${this.TEST_URL}:${this.TEST_PORT}${this.TEST_PATH.replace(/\/$/, '')}`;
+    return `${this.NORMALIZED_TEST_URL}:${this.TEST_PORT}${this.TEST_PATH.replace(/\/$/, '')}`;
   }
 };
